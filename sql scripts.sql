307ace7e00f6befd244206d9606b6f3b4bef260c
Moulya R <162025924+moulya04@users.noreply.github.com>
2024-12-01 16:54:15 +0530

    sql scripts.sql

--- sql scripts.sql
+++ sql scripts.sql
@@ -53,7 +53,7 @@ from customer
 join invoice on customer.customer_id = invoice.customer_id 
 join invoice_line on invoice.invoice_id = invoice_line.invoice_id
 where track_id in (
-		select track_id from track
+	select track_id from track
         join genre on track.genre_id = genre.genre_id
         where genre.name = "Rock")
 order by email 

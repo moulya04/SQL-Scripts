670969d7eed61d5e096214b12b2cbc3bb0c6888d
Moulya R <162025924+moulya04@users.noreply.github.com>
2024-12-01 16:53:09 +0530

    Add files via upload

--- sql scripts.sql
+++ sql scripts.sql
@@ -1,5 +1,5 @@
 create database music_store; -- to create a new database
-use music_store; -- to use the current database
+use music_store; -- to use the database
 
 -- Question set 1 - Easy
 -- Q1. Who is the senior most employee based on job title ?
@@ -91,3 +91,50 @@ select name, milliseconds
 from track 
 where milliseconds > ( select avg(milliseconds) as avg_track_length from track )
 order by milliseconds desc;
+
+-- Question set 3 - Advance
+/* Q1. Find how much amount spent by each customer on artists ?
+ Write a query to return customer name, artist name and total spent */
+ -- Using CTE 
+WITH best_selling_artist AS (
+SELECT artist.artist_id AS artist_id, artist.name AS artist_name, 
+SUM(invoice_line.unit_price * invoice_line.quantity) AS total_sales
+FROM invoice_line
+JOIN track ON track.track_id = invoice_line.track_id
+JOIN album ON album.album_id = track.album_id
+JOIN artist ON artist.artist_id = album.artist_id
+GROUP BY 1, 2
+ORDER BY 3 DESC 
+LIMIT 10
+)
+SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, 
+SUM(il.unit_price * il.quantity) AS amount_spent
+FROM invoice i 
+JOIN customer c ON c.customer_id = i.customer_id
+JOIN invoice_line il ON il.invoice_id = i.invoice_id
+JOIN track t ON t.track_id = il.track_id
+JOIN album alb ON alb.album_id = t.album_id
+JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
+GROUP BY 1, 2, 3, 4
+ORDER BY 5 DESC
+
+/* Q2. We want to find out the most popular music Genre for each country. 
+We determine the most popular genre as the genre with the highest amount of purchases.
+Write determine the most popular genre as the genre with the highest amount of purchases.
+Write a query that returns each country along with the top Genre. 
+For countries where the maximum number of purchases is shared return all Genres. */
+
+WITH popular_genre AS 
+(
+	SELECT COUNT(invoice_line.quantity) AS purchases, customer.country, genre.name, genre.genre_id,
+    ROW_NUMBER() OVER(PARTITION BY customer.country ORDER BY COUNT(invoice_line.quantity) DESC) AS RowNo
+    FROM invoice_line 
+    JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
+    JOIN customer ON customer.customer_id = invoice.customer_id
+    JOIN track ON track.track_id = invoice_line.track_id
+    JOIN genre ON genre.genre_id = track.genre_id
+    GROUP BY 2,3,4
+    ORDER BY 2 ASC, 1 DESC
+    )
+SELECT * FROM popular_genre WHERE RowNo <= 1
+
